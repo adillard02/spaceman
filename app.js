222930a6791af0e3ddcbe2717ad34d9a6cc93365
adillard02 <adillard02@gmail.com>
2024-07-21 16:37:30 -0500

    Updating

--- app.js
+++ app.js
@@ -7,17 +7,13 @@ app.use(express.static(__dirname));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-const { Pool } = require('pg');
-
-const { Pool } = require('pg');
-
 const pool = new Pool({
-    user: 'postgres',
-    host: 'localhost',
-    database: 'spaceman_scores',
-    password: 'password',
-    port: 5432,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
+
 
 async function setupDatabase() {
     try {
